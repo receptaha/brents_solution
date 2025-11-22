5b02a26998f9984d6a9d476f50f8853fdb27ea49
Recep Taha Ayvaz <109289475+receptaha@users.noreply.github.com>
2025-11-23 02:49:30 +0300

    main() updated.

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-#include "hash_functions.h"
+#include "headers/hash_functions.h"
+#include "headers/prime_functions.h"
 
 int main()
 {
-    printf("Hello world!");
+    printf("is %d a prime number?\tAnswer is %d\n", 19237123, is_prime_number(19237123));
+    printf("what is the next prime number for %d?\tAnswer is %d\n", 19237123, first_prime_number_after(19237123));
     return 0;
 }
