3e4176a6faec2c3594a4b7829f69b1f7f2f291da
Recep Taha Ayvaz <109289475+receptaha@users.noreply.github.com>
2025-11-23 02:35:34 +0300

    prime_functions.c and prime_functions.h created.

---
+++ functions/prime_functions.c
@@ -0,0 +1,20 @@
+#include "../headers/prime_functions.h"
+
+int is_prime_number(unsigned int number) {
+    unsigned int divisor = 2, counter = 0;
+    while (number != 1) {
+        if (number % divisor == 0) {
+            number /= divisor;
+            counter++;
+        }else {
+            divisor++;
+        }
+    }
+    return counter == 1 ? 1 : 0;
+}
+
+unsigned int first_prime_number_after(unsigned int number) {
+    number++;
+    while (!is_prime_number(number)) number++;
+    return number;
+}
---
+++ headers/prime_functions.h
@@ -0,0 +1,7 @@
+#ifndef PRIME_FUNCTIONS_H
+#define PRIME_FUNCTIONS_H
+
+int is_prime_number(unsigned int number);
+unsigned int first_prime_number_after(unsigned int number);
+
+#endif
